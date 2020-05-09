89d352d350fda3d7b21ec322509119240d423ab2
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-09 20:33:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  board[index].strip != ""
-  board[index] ? 
-  board.empty? ? false : 
+  board[index] ? board[index].strip != "" : false
 end
